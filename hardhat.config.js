a8fd8d18b02b0028189c1ffb080402253ceede53
krishan saini <108303973+krishannt@users.noreply.github.com>
2023-09-18 18:03:16 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const {API_URL,PRIVATE_KEY} = process.env;
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/<key>",
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
